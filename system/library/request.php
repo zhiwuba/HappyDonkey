711e271538c98143c034d62a3a029940713b66cd
zhiwuba <xiabingliu@163.com>
2014-06-12 00:31:15 +0800

    Change: PhpStorm Project.

--- system/library/request.php
+++ system/library/request.php
@@ -16,8 +16,20 @@ class Request
     public function __construct()
     {
         $_GET=$this->clean($_GET);
+        $_POST=$this->clean($_POST);
+        $_REQUEST=$this->clean($_REQUEST);
+        $_COOKIE=$this->clean($_COOKIE);
+        $_FILES=$this->clean($_FILES);
+        $_SERVER=$this->clean($_SERVER);
 
+        $this->get=$_GET;
+        $this->post=$_POST;
+        $this->request=$_REQUEST;
+        $this->cookie=$_COOKIE;
+        $this->files=$_FILES;
+        $this->server=$_SERVER;
     }
+
 
     public function clean($data)
     {
--- system/library/response.php
+++ system/library/response.php
@@ -4,4 +4,98 @@
  * User: bingxia
  * Date: 14-6-11
  * Time: 下午6:29
- */ 
+ */
+
+class Response
+{
+    private  $headers=array();
+    private  $level=0;
+    private  $output;
+
+    public function add_header($header)
+    {
+        $this->headers[]=$header; //add to end
+    }
+
+    public function redirect($url)
+    {
+        header('Location: ' . $url);
+        exit;  //???
+    }
+
+    public function set_compression($level)
+    {
+        $this->level=$level;
+    }
+
+    public function  set_output($output)
+    {
+        $this->output=$output;
+    }
+
+    private  function compress($data, $level=0)
+    {
+        $server_coding=$_SERVER['HTTP_ACCEPT_ENCODING'];
+        if ( isset($server_coding) && (strpos($server_coding, 'gzip')!=false) )
+        {
+            $encoding='gzip';
+        }
+
+        if( isset($server_coding) && (strpos($server_coding,'x-gzip')!=false) )
+        {
+            $encoding='x-gzip';
+        }
+
+        if (!isset($encoding) )
+        {
+            return $data;
+        }
+
+        if( !extension_loaded('zlib') || ini_get('zlib.output_compression') )
+        {
+            return $data;
+        }
+
+        if (headers_sent())
+        {
+            return $data;
+        }
+
+        if (connection_status())  //0: normal 1:aborted 2:timeout
+        {
+            return $data;
+        }
+
+        $this->add_header('Content-Encoding: ' . $encoding);
+
+        return  gzencode($data, (int)$level);
+    }
+
+    public function output()
+    {
+        if ( $this->output )
+        {
+            if ( $this->level )
+            {
+                $output=$this->compress($this->output, $this->level);
+            }
+            else
+            {
+                $output=$this->$this->output;
+            }
+
+            if ( !headers_send() )
+            {  //why???
+                foreach( $this->headers as $header  )
+                {
+                    header($header, true);
+                }
+            }
+
+            echo $output;
+        }
+    }
+}
+
+
+?>
