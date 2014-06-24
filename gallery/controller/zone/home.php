5084925c3f734169bc19397c1c4bb07c999dd8a4
zhiwuba <xiabingliu@163.com>
2014-06-24 20:21:01 +0800

    nothing ...

--- gallery/controller/zone/home.php
+++ gallery/controller/zone/home.php
@@ -10,15 +10,72 @@ class ControllerZoneHome extends  Controller
 {
     public function index()
     {
-        //TODO: please login first.
-        $this->template="zone/home.php";
-
         $this->load->model("zone/home");
 
-        $this->children=array("common/header","common/footer");
+        $request_uid=$this->request->get_args('uid');
 
+        if ( $this->user->is_logged() )
+        {   //如果登陆了且访问网址是自己的主页可以修改, 比人的不能修改
+            if ( ($request_uid)&&($request_uid!=$this->user->get_id()) )
+            {   //不合并到上面的if , 方便理解
+                $this->get_home_page($this->user->get_id() , false);
+                $this->get_recommend(null);
+            }
+            else
+            {
+                $this->get_home_page($this->user->get_id() , true);
+                $this->get_recommend($this->user->get_id() );
+            }
+        }
+        else
+        { //不可以修改
+            $this->get_home_page($request_uid , false);
+            $this->get_recommend(null);
+        }
+
+        $this->get_user_info($request_uid);
+
+        $this->template="zone/home.php";
+        $this->children=array("common/header","common/footer");
         $this->response->set_output($this->render());
     }
+
+    private function get_home_page($uid, $writable)
+    {
+        if ( $writable )
+        { //可写
+
+        }
+        else
+        { //仅读
+
+        }
+    }
+
+    private function get_user_info($uid)
+    {
+
+    }
+
+    private function  get_recommend($uid)
+    {
+        if ( isset($uid) )
+        { //推荐算法
+
+        }
+        else
+        { //推荐热门
+
+        }
+    }
+
+
+    public function delete_favriote()
+    {
+
+    }
+
+
 
 }
 
--- gallery/view/zone/home.php
+++ gallery/view/zone/home.php
@@ -2,12 +2,15 @@
 
 
 <div class="container" id="home_wrapper">
+
+
+
     <div id="user_section" class="shadow-box">
-        <div id="user_avatar">
-            <img src="<?php echo HTTP_SERVER . "static/image/big_avatar.jpg"?>" class="large_pic">
+        <div  class="thumbnail" style="width: 150px; height: 150px; float:left">
+            <img src="<?php echo HTTP_SERVER . "static/image/big_avatar.jpg"?>" style="width: 150px;">
         </div>
         <div id="user_detail">
-            <h1 class="shadow-text">韩柳</h1>
+            <div class="shadow-text">韩柳</div>
         </div>
     </div>
 
@@ -18,47 +21,127 @@
 
     <div id="home_main">
 
-        <div id="home_sidebar">
-            <div id="my-nav-cont" class="shadow-box">
-                <ul id="my-nav-list" class="nav nav-stacked navbar-inverse">
-                    <li class="my-nav-item active">
-                        <a href="#">我的发布</a>
-                    </li>
-                    <li class="my-nav-item">
-                        <a href="#">我的收藏</a>
-                    </li>
-                    <li class="my-nav-item">
-                        <a href="#">我的评论</a>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="ad shadow-box">
-                <h1> 广告</h1>
-            </div>
-        </div>
-
         <div id="home_news" class="div_center">
+            <div id="news_header" class="shadow-box shadow-text">
+                <div style="padding-left: 10px">最新动态</div>
+            </div>
+
             <div id="news_list">
-                <div class="a-news shadow-box">
-                    <h1>上传了什么</h1>
-                </div>
-                <div class="a-news shadow-box">
-                    <h1>更新了什么</h1>
+                <div class="panel-group" id="accordion">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" class="shadow-text">
+                                    2014.6.24 (收藏/上传)
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapseOne" class="panel-collapse collapse in">
+                            <div class="panel-body">
+                                <div class="thumb_list">
+                                    <div class="a-thumb">
+                                        <img src="/HappyDonkey/static/image/1.jpg" >
+                                    </div>
+                                    <div class="a-thumb">
+                                        <img src="/HappyDonkey/static/image/2.jpg">
+                                    </div>
+                                    <div class="a-thumb">
+                                        <img src="/HappyDonkey/static/image/3.jpg">
+                                    </div>
+                                    <div class="a-thumb">
+                                        <img src="/HappyDonkey/static/image/4.jpg">
+                                    </div>
+                                    <div class="a-thumb">
+                                        <img src="/HappyDonkey/static/image/1.jpg" >
+                                    </div>
+                                    <div class="a-thumb">
+                                        <img src="/HappyDonkey/static/image/2.jpg">
+                                    </div>
+                                    <div class="a-thumb">
+                                        <img src="/HappyDonkey/static/image/3.jpg">
+                                    </div>
+                                    <div class="a-thumb">
+                                        <img src="/HappyDonkey/static/image/4.jpg">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+                                    2014.6.23
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapseTwo" class="panel-collapse collapse in">
+                            <div class="panel-body">
+                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                                3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+                                Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et.
+                                Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                                Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table,
+                                raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+                                    2014.6.22
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapseThree" class="panel-collapse collapse in">
+                            <div class="panel-body">
+                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                                3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+                                Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et.
+                                Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                                Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table,
+                                raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
 
         <div id="home_addition">
             <div id="about_me" class="shadow-box">
-                <h1>关于我</h1>
+                <div class="column_header">
+                    <div class="head_font">我的排行</div>
+                </div>
+                <div id="popular_list" class="column_body">
+                    <?php $i=0; foreach($popular as $item ){ ?>
+                        <div class="a-popular">
+                            <a href="<?php echo $item['url']; ?>"  namecard="<?php echo $item['paint_id']?>">
+                                <div class="item_brief"><?php echo $i . ". " . $item['header'] ?></div>
+                            </a>
+                        </div>
+                        <?php $i++;} ?>
+                </div>
             </div>
 
             <div id="interesting" class="shadow-box">
-                <h1>感兴趣的</h1>
+                <div class="column_header">
+                    <div class="head_font">猜你感兴趣</div>
+                </div>
+                <div id="popular_list" class="column_body">
+                    <?php $i=0; foreach($popular as $item ){ ?>
+                        <div class="a-popular">
+                            <a href="<?php echo $item['url']; ?>"  namecard="<?php echo $item['paint_id']?>">
+                                <div class="item_brief"><?php echo $i . ". " . $item['header'] ?></div>
+                            </a>
+                        </div>
+                        <?php $i++;} ?>
+                </div>
             </div>
 
-            <div class="ad shadow-box" >
+            <div class="ad shadow-box" style="display:  node">
                 <h1>广告区</h1>
             </div>
         </div>
